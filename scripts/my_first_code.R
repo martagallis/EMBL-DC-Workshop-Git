2c1b1e492a14f1db204403096d0c8b895a7add76
Marta Ramalho <mm.ramalho19@outlook.com>
2024-01-23 14:01:39 +0000

    this is my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,18 @@
+3 + 4
+
+weight_kg <- 55
+
+my_weight_kg <- 50
+
+(my_weight_kg <- 50)
+
+my_weight_kg
+2.2*weight_kg
+
+weight_kg <- 57
+2.2*weight_kg
+
+weight_lb <- 2.2*weight_kg
+weight_kg <- 100
+
+
