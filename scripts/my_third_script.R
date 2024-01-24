81532877c463722bbd5d452e80527058e19d6c52
Marta Ramalho <mm.ramalho19@outlook.com>
2024-01-24 09:08:08 +0000

    Factoring in R

--- scripts/my_third_script.R
+++ scripts/my_third_script.R
@@ -61,3 +61,56 @@ my_list[[1]]
 my_list$names
 
 surveys[[3]]
+
+#Factors
+str(surveys)
+surveys$sex <- factor(surveys$sex)
+
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <-  factor(c("male", "female", "female", "male"))
+sex <- factor(sex,levels = c("male", "female"))
+
+#Challenge
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+nlevels(surveys$genus)
+levels(surveys$taxa)
+
+sum(surveys$taxa == "Rabbit")
+summary(surveys$taxa)
+
+# Convert factors
+as.character(sex)
+sex
+
+year_fct <- factor(c(1990,1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+# Renaming factors
+plot(surveys$sex)
+summary(surveys$sex) #NA not showing in plot because levels are only F and M
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+
+levels(sex)[3] <- "undetermined"
+levels(sex)
+sex
+
+plot(sex)
+
+#Why factor and not vector on plotting (It's using default instructions specific to R)
+#plot(c("a", "b", "a", "a", "a"))
+#plot(factor(c("a", "b", "a", "a", "a")))
+
+levels(sex)[1:2] <- c("female", "male")
+plot(sex)
+sex <- factor(sex, levels = c("undetermined", "female", "male"))
+plot(sex)
